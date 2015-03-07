2c0e4c4c8ec6ea74bdedb19c215fd9a7f19abf78
fabbemannen <fabbemannen@gmail.com>
2015-03-07 21:44:14 +0100

    Objects can be detected
    Although I need everyone to decide on how we handle the logical
    thinking. Currently Impassable objects are also detected (everything
    with the "World Object" tag and a 2D-Collider attached that got
    IsTrigger enabled.

--- Assets/Object_Close.cs
+++ Assets/Object_Close.cs
@@ -13,8 +13,14 @@ public class Object_Close : MonoBehaviour {
 	
 	}
 
-	void OnTriggerEnter(Collider other)
+	void OnTriggerEnter2D(Collider2D other)
 	{
+		//Debug.Log ("Found object with the tag " + other.tag);
+		if(other.tag == "World Object")
+		{
+			//Debug.Log ("Distance to " + other.name + " is " + Vector2.Distance(new Vector2(transform.position.x, transform.position.y), new Vector2(other.transform.position.x, other.transform.position.y)));
 
+
+		}
 	}
 }
