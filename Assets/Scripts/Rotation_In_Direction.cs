1484b257218d8a64e1ed462c21dff0dcc2953808
fabbemannen <fabbemannen@gmail.com>
2015-03-17 19:19:37 +0100

    Graphical update and sorting layers

---
+++ Assets/Scripts/Rotation_In_Direction.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotation_In_Direction : MonoBehaviour {
+
+	private Vector3 last_position;
+
+	// Use this for initialization
+	void Start () {
+		last_position = gameObject.transform.position;
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+		Vector3 gopos = gameObject.transform.position;
+		if(last_position.x != gopos.x && last_position.y != gopos.y)
+		{
+			Vector3 newPos = (gopos - last_position);
+			float angle = Mathf.Atan2(newPos.y, newPos.x) * Mathf.Rad2Deg;
+			transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+
+			//Vector3 newPos = (gopos - last_position) + gopos;
+			//transform.LookAt(newPos);
+
+			//Vector3 relativePos = (transform.position - last_position);
+			//transform.rotation = Quaternion.LookRotation(relativePos);
+		}
+		last_position = gameObject.transform.position;
+	}
+}
---
+++ Assets/Scripts/Rotation_Random.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotation_Random : MonoBehaviour {
+
+	// Use this for initialization
+	void Awake () {
+		gameObject.transform.eulerAngles.Set(transform.eulerAngles.x, transform.eulerAngles.y, Random.Range(0.0f, 360.0f));
+	}
+}
